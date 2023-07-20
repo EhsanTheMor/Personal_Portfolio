685810e605916bf409cacac1df3991e97f79e11c
Ehsan Morshedzadeh <Ehsan.morshedzadeh@gmail.com>
2023-07-20 11:32:12 +0330

    last build

--- src/util/Observer.tsx
+++ src/util/Observer.tsx
@@ -1,16 +1,21 @@
 import { Dispatch, SetStateAction } from 'react';
 
 export default function (fn: Dispatch<SetStateAction<string>>, state: string) {
-     const observer = new IntersectionObserver(entries => {
-          entries.forEach(
-               entry => {
-                    if (entry.isIntersecting) {
-                         fn(state);
-                    }
-               },
-               { threshold: 1 },
-          );
-     });
+     const observer = new IntersectionObserver(
+          entries => {
+               entries.forEach(
+                    entry => {
+                         if (entry.isIntersecting) {
+                              fn(state);
+                         }
+                    },
+                    { threshold: 1 },
+               );
+          },
+          {
+               rootMargin: '0px 0px -300px 0px',
+          },
+     );
 
      return function (state: string) {
           const tag = document.querySelector(`#${state}`);
